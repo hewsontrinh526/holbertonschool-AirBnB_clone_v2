53fb51af453e426f42338319005da0f4ba6f7e45
chelseyqc <6721@holbertonstudents.com>
2023-11-28 16:03:09 +0800

    fix: added getenv

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
@@ -10,4 +11,6 @@ class City(BaseModel, Base):
     __table__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state = relationship("State", back_populates="cities")
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        state = relationship("State", back_populates="cities")
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
@@ -12,12 +13,13 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", back_populates="state",
-                          cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """Gets cities and returns list of City instances"""
-        city_instance = storage.all(City)
-        return [city for city in city_instance.values()
-                if city.state_id == self.id]
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Gets cities and returns list of City instances"""
+            city_instance = storage.all(City)
+            return [city for city in city_instance.values()
+                    if city.state_id == self.id]
+    else:
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete-orphan")
