f4fe34dd61bcc25376eae897bf307792f83d0a72
chelseyqc <6721@holbertonstudents.com>
2023-11-28 15:15:28 +0800

    feat: updated City and State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Gets cities and returns list of City instances"""
+        city_instance = storage.all(City)
+        return [city for city in city_instance.values()
+                if city.state_id == self.id]
