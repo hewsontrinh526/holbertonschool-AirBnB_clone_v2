1adf5b1ba331d97ef485157f89236886ed3f557a
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-29 20:38:18 +1100

    Fix: Moved for DBStorage and FileStorage and fix spelling mistakes

--- models/place.py
+++ models/place.py
@@ -11,32 +11,38 @@ metadata = MetaData()
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True, default=null())
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True, default=null())
-    longitude = Column(Float, nullable=True, default=null())
-    amenity_ids = []
-
-    user = relationship("User", back_populates="places")
-    cities = relationship("City", back_populates="places")
 
     place_amenity = Table(
         'place_amenity',
         metadata,
         Column('place_id', String(60), ForeignKey('places.id'),
-               primary_key=True, nullable=False),
+            primary_key=True, nullable=False),
         Column('amenity_id', String(60), ForeignKey('amenities.id'),
-               primary_key=True, nullable=False)
+            primary_key=True, nullable=False)
     )
 
-    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+    __tablename__ = 'places'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True, default=null())
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True, default=null())
+        longitude = Column(Float, nullable=True, default=null())
+        amenity_ids = []
+
+        user = relationship("User", back_populates="places")
+        cities = relationship("City", back_populates="places")
+        reviews = relationship("Review", back_populates="place",
+                               cascade="delete, delete-orphan")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                viewonly=False)
+
+    else:
         @property
         def reviews(self):
             """
@@ -52,7 +58,7 @@ class Place(BaseModel, Base):
             return review_list
 
         @property
-        def amenites(self):
+        def amenities(self):
             """
             Getter attribute amenities that returns the list of Amenity
             instances based on the attribute amenity_ids that contains all
@@ -76,8 +82,3 @@ class Place(BaseModel, Base):
             from models import storage
             if isinstance(obj, Amenity):
                 self.amenity_ids.append(obj.id)
-    else:
-        reviews = relationship("Review", back_populates="place",
-                               cascade="delete, delete-orphan")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                viewonly=False)
