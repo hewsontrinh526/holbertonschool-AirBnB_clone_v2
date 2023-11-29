38c96fe72ddd505bfc81c9b2b13effbca0fe04e1
chelseyqc <6721@holbertonstudents.com>
2023-11-29 16:17:09 +0800

    fix: added relationship of places and amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,8 +2,22 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
+place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'),
+               primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'),
+               primary_key=True, nullable=False)
+    )
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+
+    places = relationship("Place",
+                          secondary=place_amenity,
+                          back_populates='amenities'
+                          )
