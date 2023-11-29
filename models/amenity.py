83ad026125e8aef0a3725519958ee4846dda71a4
chelseyqc <6721@holbertonstudents.com>
2023-11-29 17:57:01 +0800

    fix: pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -7,21 +7,21 @@ import os
 
 
 class Amenity(BaseModel, Base):
-       __tablename__ = 'amenities'
-       if os.getenv("HBNB_TYPE_STORAGE") == "db":
-              place_amenity = Table(
-                     'place_amenity',
-                     Base.metadata,
-                     Column('place_id', String(60), ForeignKey('places.id'),
-                            primary_key=True, nullable=False),
-                     Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                            primary_key=True, nullable=False)
+    __tablename__ = 'amenities'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column('place_id', String(60), ForeignKey('places.id'),
+                   primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                   primary_key=True, nullable=False)
               )
-              name = Column(String(128), nullable=False)
+        name = Column(String(128), nullable=False)
 
-              places = relationship("Place",
-                                   secondary=place_amenity,
-                                   back_populates='amenities'
-                                   )
-       else:
-              name = ""
+        places = relationship("Place",
+                              secondary=place_amenity,
+                              back_populates='amenities'
+                              )
+    else:
+        name = ""
