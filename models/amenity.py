45827d4e91d20bff3cd0174ae8c80777d6f0cc14
chelseyqc <6721@holbertonstudents.com>
2023-11-29 18:55:27 +0800

    fix: revert to og, did not work

--- models/amenity.py
+++ models/amenity.py
@@ -9,14 +9,6 @@ import os
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenity = Table(
-            'place_amenity',
-            Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                   primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                   primary_key=True, nullable=False)
-              )
         name = Column(String(128), nullable=False)
 
         places = relationship("Place",
