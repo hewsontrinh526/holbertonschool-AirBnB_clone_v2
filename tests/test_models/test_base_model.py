887d4fdf8f288e8e84faf5fd60da0377d11c4ed2
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-03 16:29:09 +1100

    Feat: Added unittest for DBStorage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ """
 from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from models import storage
 import unittest
 import datetime
 from uuid import UUID
@@ -97,3 +100,60 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+
+class TestDatabaseStorage(unittest.TestCase):
+    """Test cases for the DatabaseStorage class"""
+    @classmethod
+    def setUpClass(cls):
+        os.environ["HBNB_ENV"] = "test"
+        os.environ["HBNB_MYSQL_USER"] = "hbnb_dev"
+        os.environ["HBNB_MYSQL_PWD"] = "hbnb_dev_pwd"
+        os.environ["HBNB_MYSQL_HOST"] = "localhost"
+        os.environ["HBNB_MYSQL_DB"] = "hbnb_dev_db"
+        if hasattr(storage, '_DBStorage__session'):
+            storage._DBStorage__session.close()
+        else:
+            None
+        storage.reload()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.environ["HBNB_ENV"] = "production"
+        storage.reload()
+
+    def setUp(self):
+        storage.reload()
+        self.name = 'BaseModel'
+        self.value = BaseModel
+
+    def tearDown(self):
+        if isinstance(storage, DBStorage):
+            storage._DBStorage__session.close()
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_default(self):
+        i = self.value()
+        self.assertEqual(type(i), self.value)
+
+    def test_save(self):
+        i = self.value()
+        i.save()
+        key = "{}.{}".format(type(i).__name__, i.id)
+        obj = storage.all()[key]
+        self.assertEqual(obj.to_dict(), i.to_dict())
+
+    def test_delete(self):
+        i = self.value()
+        i.save()
+        i_id = i.id
+        key = "{}.{}".format(type(i).__name__, i_id)
+        i.delete()
+        self.assertNotIn(key, storage.all())
+
+
+if __name__ == '__main__':
+    unittest.main()
