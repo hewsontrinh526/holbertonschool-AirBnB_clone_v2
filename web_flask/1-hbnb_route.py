cdbfeb0a7536a9ed5d79c4ca7e867f8db7bfb0a6
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-11 20:43:33 +1100

    Fix: PEP8 validation checks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-A script that starts a Flask web application with route /hbnb
+A script that starts a Flask web application with different routes like
+/hbnb and /
 """
 from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
@@ -13,6 +15,7 @@ def hello():
     Displays a Hello string
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
