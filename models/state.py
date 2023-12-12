8acf78672e2a30d0156c9a86c150ff885611ed11
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-12 16:35:18 +1100

    Feat: Moved getter method to else statement to return list of city

--- models/state.py
+++ models/state.py
@@ -13,6 +13,9 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete-orphan")
+    else:
         @property
         def cities(self):
             """Gets cities and returns list of City instances"""
@@ -20,7 +23,3 @@ class State(BaseModel, Base):
             city_instance = storage.all(City)
             return [city for city in city_instance.values()
                     if city.state_id == self.id]
-        cities = relationship("City", back_populates="state",
-                              cascade="all, delete-orphan")
-    else:
-        name = ""
