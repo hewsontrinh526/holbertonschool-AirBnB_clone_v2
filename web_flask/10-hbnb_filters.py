15aefff66371e55cf7a74382db6ccfd609a41b79
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-14 17:38:34 +1100

    Fix: Revert changes

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-"""
+""" Nameless Module for Flask """
 from flask import Flask, render_template
-from models import storage
+from models.__init__ import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 
-
 app = Flask(__name__)
+
+
+@app.route("/filters", strict_slashes=False)
+def filters():
+    """ Print out HTML lisitng out all Cities by their States """
+    states = storage.all(State).values()
+    states = sorted(states.values(), key=lambda state: state.name)
+    cities = storage.all(City).values()
+    cities = sorted(cities.values(), key=lambda city: city.name)
+    amenities = storage.all(Amenity)
+    amenities = sorted(amenities.values(),
+                       key=lambda amenities: amenities.name)
+
+    return render_template('10-hbnb_filters.html', states=states,
+                           cities=cities, amenities=amenities)
 
 
 @app.teardown_appcontext
@@ -18,30 +30,6 @@ def teardown_db(exception):
     Closes the storage
     """
     storage.close()
-
-
-@app.route("/hbnb_filters", strict_slashes=False)
-def states_list():
-    """
-    Displays an HTML page with a list of all states
-    """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda amenity: amenity.name)
-
-    specific_state = None
-
-    if id:
-        specific_state = next((state for state in states if state.id == id),
-                              None)
-        if specific_state is not None:
-            specific_state.cities = sorted(specific_state.cities,
-                                           key=lambda city: city.name)
-        states = None
-
-    return render_template('10-hbnb_filters.html', states=states,
-                           specific_state=specific_state, amenities=amenities)
 
 
 if __name__ == "__main__":
